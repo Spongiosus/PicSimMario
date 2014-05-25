52e9d2f90598de5b0ea537e7f2bf771a4e95ea31
Spongiosus <qhilihqfred@googlemail.de>
2014-05-25 16:33:11 +0200

    replaced befehlszeilennummer with PCL

--- src/PicSimulator/Processor.java
+++ src/PicSimulator/Processor.java
@@ -9,19 +9,17 @@ public class Processor {
         String dateiName = "C://test.lst";
         Parser.ladeDatei(dateiName);
         
-        int befehlsZeilenNummer = 0;
         
         int byte1 = 0;
         int byte2 = 0;  
         
 
-        while(Parser.befehlszeilen[befehlsZeilenNummer] != null)
+        while(Parser.befehlszeilen[Register.PCL] != null)
         {
         	
-        	befehlsZeilenNummer = Register.PCL;
         	
-        	byte1 = Integer.parseInt(Parser.befehlszeilen[befehlsZeilenNummer].substring(5, 7), 16);
-        	byte2 = Integer.parseInt(Parser.befehlszeilen[befehlsZeilenNummer].substring(7, 9), 16);   	
+        	byte1 = Integer.parseInt(Parser.befehlszeilen[Register.PCL].substring(5, 7), 16);
+        	byte2 = Integer.parseInt(Parser.befehlszeilen[Register.PCL].substring(7, 9), 16);   	
         	
         	String aktuellerBefehl = Decoder.erkenneBefehl(byte1, byte2);
         	System.out.printf(aktuellerBefehl + "		Arg1:	0x%x	Arg2:	0x%x\n",byte1,byte2);
